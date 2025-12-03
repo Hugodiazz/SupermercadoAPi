d130eb2f69638778bbb765371d24fb4cdc42098c
Hugodiazz <hugozaid2002@gmail.com>
2025-12-03 11:36:09 -0600

    Project creation

---
+++ SupermercadoAPI/src/test/java/com/hugodiazz/SupermercadoAPI/SupermercadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugodiazz.SupermercadoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
