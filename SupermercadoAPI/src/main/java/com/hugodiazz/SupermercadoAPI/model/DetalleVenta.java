138aa69f35338d1b11138e9bd528ee54f8931f68
Hugodiazz <hugozaid2002@gmail.com>
2025-12-03 11:36:26 -0600

    Models

---
+++ SupermercadoAPI/src/main/java/com/hugodiazz/SupermercadoAPI/model/DetalleVenta.java
@@ -0,0 +1,27 @@
+package com.hugodiazz.SupermercadoAPI.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Venta  venta;
+
+    @ManyToOne
+    private Producto producto;
+    private Integer cantProd;
+    private Double precio;
+}
